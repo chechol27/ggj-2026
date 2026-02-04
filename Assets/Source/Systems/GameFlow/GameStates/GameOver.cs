ec94b8f5067047bf6bbe06bf015d679a10064e21
Sergio <sergio.penaloa.herrera@gmail.com>
2026-02-04 14:08:42 -0500

    Additive HUDs and decoupled scores

--- Assets/Source/Systems/GameFlow/GameStates/GameOver.cs
+++ Assets/Source/Systems/GameFlow/GameStates/GameOver.cs
@@ -1,53 +1,14 @@
-﻿using System.Collections;
-using Unity.Entities;
-using UnityEngine;
-using UnityEngine.Networking;
-
-public class GameOver : GameStage
+﻿public class GameOver : GameStage
 {
-    private string URL =
-        "https://script.google.com/macros/s/AKfycbxSoO2gksEcMHaqklyrisA4w5j93bwKHJpq8Ir9RhQZWqmI3i0WQkYgI7gUBRlRiL7W/exec";
-
-    
     public override void OnStateEnter()
     {
         GameServices.Get<HUD>().SetHUDUI<GameOverHUD>();
         GameServices.Get<GameFlow>().SetPause(true);
-        SendAndResetPoints();
-        //Canvas de perder
-    }
-
-    private void SendAndResetPoints()
-    {
-        if (!PlayerPrefs.HasKey("Username")) return;
-        StartCoroutine(SendScore(GameServices.Get<Game>().Points));
+        GameServices.Get<Score>().SendAndResetPoints();
     }
 
     public override void OnStateExit()
     {
         GameServices.Get<GameFlow>().SetPause(false);
-    }
-    
-    IEnumerator SendScore(int points)
-    {
-        WWWForm form = new WWWForm();
-
-        form.AddField("uid", PlayerPrefs.GetString("Username", "null"));
-        form.AddField("score", points);
-
-        using (UnityWebRequest www = UnityWebRequest.Post(URL, form))
-        {
-            yield return www.SendWebRequest();
-
-            if (www.result != UnityWebRequest.Result.Success)
-            {
-                Debug.LogError("Error: " + www.error);
-            }
-            else
-            {
-                Debug.Log("Respuesta de Google Sheets: " + www.downloadHandler.text);
-                Debug.Log("Data enviada");
-            }
-        }
     }
 }
---
+++ Assets/Source/Systems/Score/Score.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class Score : MonoBehaviour, IGameService
+{
+    const string URL = "https://script.google.com/macros/s/AKfycbxSoO2gksEcMHaqklyrisA4w5j93bwKHJpq8Ir9RhQZWqmI3i0WQkYgI7gUBRlRiL7W/exec";
+
+    IEnumerator SendScore(int points)
+    {
+        WWWForm form = new ();
+
+        form.AddField("uid", PlayerPrefs.GetString("Username", "null"));
+        form.AddField("score", points);
+
+        using (UnityWebRequest www = UnityWebRequest.Post(URL, form))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.result != UnityWebRequest.Result.Success)
+            {
+                Debug.LogError("Error: " + www.error);
+            }
+            else
+            {
+                Debug.Log("Respuesta de Google Sheets: " + www.downloadHandler.text);
+                Debug.Log("Data enviada");
+            }
+        }
+    }
+    
+    public void SendAndResetPoints()
+    {
+        if (!PlayerPrefs.HasKey("Username")) return;
+        StartCoroutine(SendScore(GameServices.Get<Game>().Points));
+    }
+}
